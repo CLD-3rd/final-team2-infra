4c818d298f570213ad642a1385ee7bbf2a185139
seoulcloud <integrall92@gmail.com>
2025-08-13 11:50:44 +0900

    feat: argocd Helm 차트에 ALB 호스트 이름을 로컬 변수로 설정하고 Route53 CNAME 레코드에 적용

--- main.tf
+++ main.tf
@@ -558,16 +558,19 @@ data "kubernetes_ingress_v1" "argocd" {
   ]
 }
 
+locals {
+  argocd_alb_hostname = try(data.kubernetes_ingress_v1.argocd.status[0].load_balancer[0].ingress[0].hostname, "")
+}
+
 # Route53 CNAME for ArgoCD: argocd.<domain> → ALB hostname
 resource "aws_route53_record" "argocd" {
+  count   = local.argocd_alb_hostname != "" ? 1 : 0
   zone_id = aws_route53_zone.main.zone_id
   name    = "argocd.${var.domain_name}"
   type    = "CNAME"
   ttl     = 300
 
-  records = [
-    data.kubernetes_ingress_v1.argocd.status[0].load_balancer[0].ingress[0].hostname,
-  ]
+  records = [local.argocd_alb_hostname]
 
   depends_on = [
     aws_route53_zone.main,
